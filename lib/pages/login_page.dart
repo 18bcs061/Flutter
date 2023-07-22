c8af938836e8f7a85942ae55a7521a60f0cc533c
abhishek <abhishek.kumar@mandsc.com>
2023-07-22 16:02:26 +0530

    Login form with onclick function

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -5,14 +7,40 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-              fontSize: 40, fontWeight: FontWeight.bold, color: Colors.red),
-        ),
-      ),
-    );
+    return Material(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login.png",
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "Welcome",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+              child: Column(children: [
+                TextFormField(
+                    decoration: const InputDecoration(
+                        hintText: "Enter UserName", labelText: "UserName")),
+                TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        hintText: "Enter Password", labelText: "Password")),
+                const SizedBox(height: 40),
+                ElevatedButton(
+                    onPressed: () {
+                      print("Login sucessfully");
+                    },
+                    style: TextButton.styleFrom(),
+                    child: const Text("Login"))
+              ]),
+            )
+          ],
+        ));
   }
 }

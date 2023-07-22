9ddb0dbd5ab854787e266c99aa8ac254a1be12b3
abhishek <abhishek.kumar@mandsc.com>
2023-07-22 22:14:12 +0530

    Stateful , Animated container and future delay

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,9 +3,17 @@
 import 'package:first_flutter_app/utils/routes.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  // ignore: non_constant_identifier_names
+  bool ChangeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -18,9 +26,10 @@ class LoginPage extends StatelessWidget {
                 fit: BoxFit.cover,
               ),
               const SizedBox(height: 20),
-              const Text(
-                "Welcome",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              Text(
+                "Welcome $name",
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
               ),
               const SizedBox(height: 20),
               Padding(
@@ -28,20 +37,57 @@ class LoginPage extends StatelessWidget {
                     const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
                 child: Column(children: [
                   TextFormField(
-                      decoration: const InputDecoration(
-                          hintText: "Enter UserName", labelText: "UserName")),
+                    decoration: const InputDecoration(
+                        hintText: "Enter UserName", labelText: "UserName"),
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
+                  ),
                   TextFormField(
                       obscureText: true,
                       decoration: const InputDecoration(
                           hintText: "Enter Password", labelText: "Password")),
                   const SizedBox(height: 40),
-                  ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, MyRoutes.homeRoute);
-                      },
-                      style: TextButton.styleFrom(
-                          minimumSize: const Size(120, 40)),
-                      child: const Text("Login"))
+                  InkWell(
+                    onTap: () async {
+                      setState(() {
+                        ChangeButton = true;
+                      });
+                      await Future.delayed(const Duration(seconds: 1));
+                      // ignore: use_build_context_synchronously
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    },
+                    child: AnimatedContainer(
+                      width: ChangeButton ? 50 : 150,
+                      height: 50,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius:
+                              BorderRadius.circular(ChangeButton ? 50 : 10)),
+                      duration: const Duration(seconds: 1),
+                      child: ChangeButton
+                          ? const Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : const Text(
+                              "Login",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18),
+                            ),
+                    ),
+                  ),
+                  // ElevatedButton(
+                  //     onPressed: () {
+                  //       Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //     },
+                  //     style: TextButton.styleFrom(
+                  //         minimumSize: const Size(120, 40)),
+                  //     child: const Text("Login"))
                 ]),
               )
             ],
